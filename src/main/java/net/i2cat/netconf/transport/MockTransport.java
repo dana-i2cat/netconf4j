eb2d7882fc82051cb3c45f90ae863b5b179868b7
carlos <carlos@mint>
2010-09-30 17:22:23 +0000

    Modifications to run test in protocol project

--- src/main/java/net/i2cat/netconf/transport/MockTransport.java
+++ src/main/java/net/i2cat/netconf/transport/MockTransport.java
@@ -1,3 +1,19 @@
+/**
+ * This file is part of Netconf4j.
+ *
+ * Netconf4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Netconf4j is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Netconf4j. If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.i2cat.netconf.transport;
 
 import java.util.ArrayList;
@@ -99,7 +115,7 @@ public class MockTransport implements Transport {
 			}
 			if (op == Operation.EDIT_CONFIG) {
 			}
-			if (op == Operation.GET) {
+			if (op.equals(Operation.GET)) {
 
 				if (query.getFilter() != null && query.getFilterType() != null) {
 					if (context.getActiveCapabilities().contains(Capability.XPATH)) {
